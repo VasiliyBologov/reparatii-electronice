de9f1560c6fb41754eecdbc3062b2b77b34a79cd
Ivan Organ <ivanorgan@gmail.com>
2023-06-23 15:18:26 +0300

    add services

--- src/pages/Servicii.js
+++ src/pages/Servicii.js
@@ -20,13 +20,21 @@ function Servicii() {
           </Grid>
           <Grid item xs={12} sm={6} md={6} sx={{textAlign: 'right'}}>
             <Typography variant="h5" color="text.primary">
-            Текст какой то который описывает что ты тут делаешь
+            Reparații PC/Laptop. Efectuăm reparații PC/Laptopuri 
             </Typography>
             <Typography variant="h5" color="text.primary">
             Цена: 
             </Typography>
             <Typography variant="body2" color="text.secondary">
-            что то ещё
+            DIAGNOSTICARE PC/LAPTOP
+            Diagnosticare Laptop / PC gratuita
+            REPARATII PC/LAPTOP
+            Efectuam orice tip de reparatii. Efectuam reparatii cu piese originale si compatibile.
+            Reparatii Alimentatoare Laptopuri. Reparatii Surse ATX. Reparatii diverse electronice
+            CURATARE PC/LAPTOP
+            Curatire si schimbare pasta termica are avantajele semnificative:
+            Prelungirea duratei de functionare; Diminuarea apararitiei unui defect;
+
             </Typography>
           </Grid>
         </Grid>
@@ -35,13 +43,13 @@ function Servicii() {
         <Grid container spacing={2} sx={{textAlign: 'left'}}>
           <Grid item xs={12} sm={6} md={6} sx={{textAlign: 'дуае'}}>
             <Typography variant="h5" color="text.primary">
-            Текст какой то который описывает что ты тут делаешь
+            Reparatii imprimante/Multifunctionale
             </Typography>
             <Typography variant="h5" color="text.primary">
             Цена: 
             </Typography>
             <Typography variant="body2" color="text.secondary">
-            что то ещё
+            reparatia imprimante/ Multifunctionale de model: CANON, HP, SAMSUNG, EPSON, XEROX, KYOCERA, PANASONIC, BROTHER, KONICA MINOLTA, LENOVO si alte.
             </Typography>
           </Grid>
           <Grid item xs={12} sm={4} md={4} sx={{textAlign: 'center'}}>
@@ -73,13 +81,13 @@ function Servicii() {
           </Grid>
           <Grid item xs={12} sm={6} md={6} sx={{textAlign: 'right'}}>
             <Typography variant="h5" color="text.primary">
-            Текст какой то который описывает что ты тут делаешь
+            Reincarcare cartuse/ Regenerare cartuse
             </Typography>
             <Typography variant="h5" color="text.primary">
             Цена: 
             </Typography>
             <Typography variant="body2" color="text.secondary">
-            что то ещё
+            Reincarcare si regenerare cartuse de model: CANON, HP, SAMSUNG, EPSON, XEROX, KYOCERA, PANASONIC, BROTHER, KONICA MINOLTA, LENOVO si alte.
             </Typography>
           </Grid>
         </Grid>
@@ -88,13 +96,16 @@ function Servicii() {
         <Grid container spacing={2} sx={{textAlign: 'left'}}>
           <Grid item xs={12} sm={6} md={6} sx={{textAlign: 'дуае'}}>
             <Typography variant="h5" color="text.primary">
-            Текст какой то который описывает что ты тут делаешь
+            Reparatii TV/Monitor LED si LCD
             </Typography>
             <Typography variant="h5" color="text.primary">
             Цена: 
             </Typography>
             <Typography variant="body2" color="text.secondary">
-            что то ещё
+            Reparatii Televizoare LED
+            Reparatii Televizoare LCD
+            Reparatii bloc de alimentare TV/ Monitor
+            Reparatii Monitoare LED, LCD
             </Typography>
           </Grid>
           <Grid item xs={12} sm={4} md={4} sx={{textAlign: 'center'}}>
